d327b0b592292b0ab5df17a3807978979aa7aa79
天天向上 <john.yi@qq.com>
2020-04-17 09:57:04 +0800

    httpclient调用例子

--- his-web-parent/his-prescription/src/main/java/cn/bdqn/his/prescription/controller/DemoController.java
+++ his-web-parent/his-prescription/src/main/java/cn/bdqn/his/prescription/controller/DemoController.java
@@ -1,13 +1,27 @@
 package cn.bdqn.his.prescription.controller;
 
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cn.bdqn.his.common.http.HttpClientHelper;
 import cn.bdqn.his.common.response.Response;
+import cn.bdqn.his.common.response.ResponseEnum;
 
 /**
  * api调用demo
@@ -16,6 +30,7 @@ import cn.bdqn.his.common.response.Response;
  */
 @Api
 @RestController
+@Slf4j
 public class DemoController {
 	@Value("${server.medicine.url}")
 	private String serverMedicineUrl;
@@ -24,6 +39,35 @@ public class DemoController {
 
 	@Autowired private HttpClientHelper httpClientHelper;
 
+	@GetMapping("api/medicine/findBy")
+	public Response medicineFindBy(Integer pageNum,Integer pageSize, Integer typeId, String name, HttpServletRequest request) {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		try {
+			httpClient = HttpClientBuilder.create().build();
+			StringBuffer params = new StringBuffer("?");
+			params.append("pageNum=").append(pageNum==null?1:pageNum);
+			params.append("&pageSize=").append(pageSize==null?2:pageSize);
+			params.append("&typeId=").append(typeId==null?1:typeId);
+			params.append("&name=").append(name==null?"":name.trim());
+			String ssoCookies = (String) request.getAttribute("ssoCookies");
+			HttpGet httpGet = new HttpGet(serverMedicineUrl + "/api/medicines/findBy" + params);
+			httpGet.addHeader("Cookie",ssoCookies);//带上cookie，表明身份
+			response = httpClient.execute(httpGet);
+			if(response.getStatusLine().getStatusCode()==200) {
+				HttpEntity entity = response.getEntity();
+				String result = EntityUtils.toString(entity);
+				return new ObjectMapper().readValue(result, Response.class);
+			}
+		} catch (Exception e) {
+			log.error("调用药品模块错误",e);
+		} finally {
+			HttpClientUtils.closeQuietly(response);
+			HttpClientUtils.closeQuietly(httpClient);
+		}
+		return new Response(ResponseEnum.ERROR).setResponseBody("出错了");
+	}
+	
 	@GetMapping("api/masterdata/users")
 	public Response getMasterdataUsers() {
 		return httpClientHelper.getForResponse(serverMasterdataUrl + "/api/masterdata/users");
